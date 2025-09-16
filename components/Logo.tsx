cd3fb20766bfbcba3bff3a0b289dc411d0112a43
RedcoatRO <lucian.hainarosie@gmail.com>
2025-09-16 08:37:23 +0300

    Actualizare: versiune nouă a aplicației

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -11,7 +11,7 @@ const Logo: React.FC<IconProps> = (props) => (
     <svg viewBox="0 0 180 26" xmlns="http://www.w3.org/2000/svg" {...props}>
         {/* Folosim clase Tailwind pentru a permite adaptarea la tema dark. 
             'font-sans' asigură consistența cu restul site-ului. */}
-        <text x="0" y="20" className="font-sans text-[20px] font-bold fill-primary">
+        <text x="0" y="20" className="font-sans text-[20px] font-bold fill-text-main dark:fill-white">
             Open Road
             {/* Partea "Leasing" își schimbă culoarea în dark mode */}
             <tspan className="fill-muted dark:fill-gray-400"> Leasing</tspan>
