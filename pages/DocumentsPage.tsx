b67a0f1ca8cd4ec698f509d056e326a739dc412d
RedcoatRO <lucian.hainarosie@gmail.com>
2025-10-17 09:37:43 +0300

    Actualizare: versiune nouă a aplicației

--- pages/DocumentsPage.tsx
+++ pages/DocumentsPage.tsx
@@ -8,22 +8,22 @@ const documentsData = [
     {
         title: "Draft Contract de Leasing Operațional",
         description: "Un model standard al contractului nostru de leasing operațional. Acest document este furnizat în scop informativ.",
-        fileUrl: "https://firebasestorage.googleapis.com/v0/b/open-road-leasing.appspot.com/o/documents%2FDraft_Contract_Leasing_Operational.pdf?alt=media",
+        fileUrl: "https://firebasestorage.googleapis.com/v0/b/open-road-leasing.firebasestorage.app/o/documents%2FCONTRACT%20DE%20LEASING%20OPERA%C8%9AIONAL.pdf?alt=media&token=8e58e5f5-14bc-47ba-b11c-a10b951f7df1",
     },
     {
         title: "Termeni și Condiții Generale",
         description: "Documentul complet care guvernează termenii și condițiile serviciilor Open Road Leasing.",
-        fileUrl: "https://firebasestorage.googleapis.com/v0/b/open-road-leasing.appspot.com/o/documents%2FTermeni_si_Conditii_Generale.pdf?alt=media",
+        fileUrl: "https://firebasestorage.googleapis.com/v0/b/open-road-leasing.firebasestorage.app/o/documents%2FTERMENI%20%C8%98I%20CONDI%C8%9AII%20GENERALE.pdf?alt=media&token=6e4933ae-6ef8-4699-a298-1b0edfffac8a",
     },
     {
         title: "Politica de Confidențialitate (GDPR)",
         description: "Detalii complete despre cum colectăm, utilizăm și protejăm datele dumneavoastră personale.",
-        fileUrl: "https://firebasestorage.googleapis.com/v0/b/open-road-leasing.appspot.com/o/documents%2FPolitica_de_Confidentialitate_GDPR.pdf?alt=media",
+        fileUrl: "https://firebasestorage.googleapis.com/v0/b/open-road-leasing.firebasestorage.app/o/documents%2FPOLITICA%20DE%20CONFIDEN%C8%9AIALITATE.pdf?alt=media&token=82ee08eb-2caf-4f20-a956-bc3621f5b0ae",
     },
     {
         title: "Ghid de Utilizare Vehicul",
         description: "Un ghid practic cu recomandări pentru întreținerea și utilizarea corectă a vehiculelor din flota noastră.",
-        fileUrl: "https://firebasestorage.googleapis.com/v0/b/open-road-leasing.appspot.com/o/documents%2FGhid_de_Utilizare_Vehicul.pdf?alt=media",
+        fileUrl: "https://firebasestorage.googleapis.com/v0/b/open-road-leasing.firebasestorage.app/o/documents%2FGhid%20de%20Utilizare%20Vehicul.pdf?alt=media&token=a40945a2-17cc-4884-8c1c-845439572884",
     },
 ];
 
